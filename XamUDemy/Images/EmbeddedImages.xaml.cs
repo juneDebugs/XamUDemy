561d367059f64206e9ed3ea901f93f368b3a119e
JuneCho23 <junecho.ucb@gmail.com>
2017-08-07 10:31:38 -0700

    Created Udemy Project

---
+++ XamUDemy/Images/EmbeddedImages.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy
+{
+    public partial class EmbeddedImages : ContentPage
+    {
+        public EmbeddedImages()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamUDemy/XamUDemyPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamUDemy
+{
+    public partial class XamUDemyPage : ContentPage
+    {
+        public XamUDemyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
