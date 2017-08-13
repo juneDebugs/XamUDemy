18a8083e7d42db39a4f1fc06d8381703dba3048f
JuneCho23 <junecho.ucb@gmail.com>
2017-08-13 16:39:07 -0700

    Create Quotes Page

---
+++ XamUDemy/Pages/QuotesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Pages
+{
+    public partial class QuotesPage : ContentPage
+    {
+        public QuotesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
