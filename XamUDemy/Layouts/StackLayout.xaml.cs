471f07086ad41078a03663c3a7bf930856159387
JuneCho23 <junecho.ucb@gmail.com>
2017-08-13 13:27:08 -0700

    Binding Example Finished

--- XamUDemy/Layouts/StackLayout.xaml.cs
+++ XamUDemy/Layouts/StackLayout.xaml.cs
@@ -15,9 +15,9 @@ namespace XamUDemy.Layouts
             slider.Value = 0.5;
         }
 
-		void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
-		{
-            label.Text = String.Format("Value is {0:F2}", e.NewValue);
-		}
+		//void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+		//{
+  //          label.Text = String.Format("Value is {0:F2}", e.NewValue);
+		//}
     }
 }
