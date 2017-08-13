8cb032c470a3795adcbc4604b3b55061f54a13fe
JuneCho23 <junecho.ucb@gmail.com>
2017-08-13 15:43:53 -0700

    Device Differences OnPlatform

--- XamUDemy/Layouts/StackLayout.xaml.cs
+++ XamUDemy/Layouts/StackLayout.xaml.cs
@@ -13,11 +13,23 @@ namespace XamUDemy.Layouts
             InitializeComponent();
 
             slider.Value = 0.5;
+
+            if (Device.OS == TargetPlatform.iOS)
+                //Thickness= Left, Top, Right, Bottom
+                Padding = new Thickness(0, 20, 0, 0);
+            else if (Device.OS == TargetPlatform.Android)
+                Padding = new Thickness(10, 20, 0, 0);
+
+            Padding = Device.OnPlatform(
+                iOS: new Thickness(0,20,0,0),
+                Android: new Thickness(10,20,0,0)
+            );
         }
+        //No more need for event handler because of Binding Context
 
 		//void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
 		//{
-  //          label.Text = String.Format("Value is {0:F2}", e.NewValue);
+        //  label.Text = String.Format("Value is {0:F2}", e.NewValue);
 		//}
     }
 }
