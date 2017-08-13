83580cd44d1637a4d0caf172d7f6865fc63f771a
JuneCho23 <junecho.ucb@gmail.com>
2017-08-13 12:22:06 -0700

    ButtonPage

---
+++ XamUDemy/Pages/ButtonPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Pages
+{
+    public partial class ButtonPage : ContentPage
+    {
+        public ButtonPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
