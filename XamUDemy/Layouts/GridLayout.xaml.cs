d31b5aaf9dab61c508664c0f4762b7b93d25af52
JuneCho23 <junecho.ucb@gmail.com>
2017-08-14 10:49:10 -0700

    Created GridLayout

---
+++ XamUDemy/Layouts/GridLayout.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Layouts
+{
+    public partial class GridLayout : ContentPage
+    {
+        public GridLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
