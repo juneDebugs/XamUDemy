2a32ca4775985d5fbbeb95953acc95112a309c4d
JuneCho23 <junecho.ucb@gmail.com>
2017-08-15 09:57:21 -0700

    Created AbsoluteLayout for C#

--- XamUDemy/Layouts/AbsoluteLayout.xaml.cs
+++ XamUDemy/Layouts/AbsoluteLayout.xaml.cs
@@ -10,6 +10,15 @@ namespace XamUDemy.Layouts
         public AbsoluteLayout()
         {
             InitializeComponent();
+
+            var layout = new AbsoluteLayout();
+            Content = layout;
+
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add(aquaBox,
+                                new Rectangle(0, 0, 1, 1),
+                                AbsoluteLayoutFlags.All);
+
         }
     }
 }
