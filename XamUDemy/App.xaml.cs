2e0390f2f67a648f92925603cb7b0c3cffa1127e
JuneCho23 <junecho.ucb@gmail.com>
2017-08-14 20:31:33 -0700

    Finished CellPhonePage

--- XamUDemy/App.xaml.cs
+++ XamUDemy/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamUDemy
         {
             InitializeComponent();
 
-            MainPage = new QuotesPage();
+            MainPage = new CellDialPage();
         }
 
         protected override void OnStart()
