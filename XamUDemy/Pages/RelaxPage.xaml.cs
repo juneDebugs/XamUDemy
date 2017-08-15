b942059aec0c3bfb42dbde3bdf5f0fa8c50748cf
JuneCho23 <junecho.ucb@gmail.com>
2017-08-15 14:38:35 -0700

    Created RelaxPage

---
+++ XamUDemy/Pages/RelaxPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Pages
+{
+    public partial class RelaxPage : ContentPage
+    {
+        public RelaxPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
