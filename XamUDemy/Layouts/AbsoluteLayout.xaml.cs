04307bc3b83aa86e69f1ddd8856d7a7e472cfe6f
JuneCho23 <junecho.ucb@gmail.com>
2017-08-15 11:06:57 -0700

    Created Button on CreditPAge

--- XamUDemy/Layouts/AbsoluteLayout.xaml.cs
+++ XamUDemy/Layouts/AbsoluteLayout.xaml.cs
@@ -11,16 +11,16 @@ namespace XamUDemy.Layouts
         {
             InitializeComponent();
 
-            var layout = new AbsoluteLayout();
-            Content = layout;
+            //var layout = new AbsoluteLayout();
+            //Content = layout;
 
-            var aquaBox = new BoxView { Color = Color.Aqua };
-            layout.Children.Add(aquaBox,
-                                new Rectangle(0, 0, 1, 1),
-                                AbsoluteLayoutFlags.All);
+            //var aquaBox = new BoxView { Color = Color.Aqua };
+            //layout.Children.Add(aquaBox,
+            //                    new Rectangle(0, 0, 1, 1),
+            //                    AbsoluteLayoutFlags.All);
 
-            AbsoluteLayout.SetLayoutBounds(aquaBox, new Rectangle(0, 0, 1, 1));
-            AbsoluteLayout.SetLayoutFlags(aquaBox, AbsoluteLayoutFlags.HeightProportional);
+            //AbsoluteLayout.SetLayoutBounds(aquaBox, new Rectangle(0, 0, 1, 1));
+            //AbsoluteLayout.SetLayoutFlags(aquaBox, AbsoluteLayoutFlags.HeightProportional);
         }
     }
 }
