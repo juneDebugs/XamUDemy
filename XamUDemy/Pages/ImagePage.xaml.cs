82f142d059175c9fb2eeeabf7978776ac8b41069
JuneCho23 <junecho.ucb@gmail.com>
2017-08-16 11:16:12 -0700

    Created 2 types of ImageSource

--- XamUDemy/Pages/ImagePage.xaml.cs
+++ XamUDemy/Pages/ImagePage.xaml.cs
@@ -10,20 +10,28 @@ namespace XamUDemy.Pages
         public ImagePage()
         {
             InitializeComponent();
+			//Dirty Way
+			//var imageSource= (UriImageSource) ImageSource.FromUri(new Uri("http://..."));
 
-            //Cleaner Way
-            var imageSource = new UriImageSource { Uri = new Uri("http://...") };
+			//Cleaner Way
+			var imageSource = new UriImageSource { Uri = new Uri("http://...") };
 
-            //DirtyWay
-            //var imageSource= (UriImageSource) ImageSource.FromUri(new Uri("http://..."));
 
             //ImageSource Properties
 
-            //CachingEnabled is a Bool and is automatically enabled
+            //CachingEnabled is a Bool 
+            //Default is automatically enabled
+            //Need to set this to false if you want to dynamically change images
             imageSource.CachingEnabled = false;
 
+            //CacheValidity sets how long the cache is valid
+            //Default is 24hours
+            //This is how to change the time-span
+            imageSource.CacheValidity = TimeSpan.FromHours(1);
+            //This is 1hr timeSpan
 
-
+            image.Source = imageSource;
+            image.Source = "http://...";
         }
     }
 }
