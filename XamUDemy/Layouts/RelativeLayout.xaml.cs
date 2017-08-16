e6ee5d78e880c36607447adf68efd6001c6b846a
JuneCho23 <junecho.ucb@gmail.com>
2017-08-16 13:20:55 -0700

    Fixed RelativeLayout C#

--- XamUDemy/Layouts/RelativeLayout.xaml.cs
+++ XamUDemy/Layouts/RelativeLayout.xaml.cs
@@ -11,19 +11,19 @@ namespace XamUDemy.Layouts
         {
             InitializeComponent();
 
-            //var layout = new RelativeLayout();
-            //Content = layout;
+            var layout = new Xamarin.Forms.RelativeLayout();
+            Content = layout;
 
-            //var aquaBox = new BoxView { Color = Color.Aqua };
-            //layout.Children.Add
-            //      (aquaBox,
-            //       widthConstraint: Constraint.RelativeToParent(parent => parent.Width),
-            //       heightConstraint: Constraint.RelativeToParent(parent => parent.Height * 0.3));
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add
+                  (aquaBox,
+                   widthConstraint: Constraint.RelativeToParent(parent => parent.Width),
+                   heightConstraint: Constraint.RelativeToParent(parent => parent.Height * 0.3));
 
-            //var silverBox = new BoxView { Color = Color.Silver };
-            //layout.Children.Add
-                  //(silverBox,
-                   //yConstraint: Constraint.RelativeToView(aquaBox, (RelativeLayout, element) => element.Height + 20));
+            var silverBox = new BoxView { Color = Color.Silver };
+            layout.Children.Add
+                  (silverBox,
+                   yConstraint: Constraint.RelativeToView(aquaBox, (RelativeLayout, element) => element.Height + 20));
              
         }
     }
