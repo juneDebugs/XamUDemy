db0b9459fc979dd05728680bca1e7ce76e1999ae
JuneCho23 <junecho.ucb@gmail.com>
2017-08-16 10:46:52 -0700

    Created Images

---
+++ XamUDemy/Images/Images.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Images
+{
+    public partial class Images : ContentPage
+    {
+        public Images()
+        {
+            InitializeComponent();
+        }
+    }
+}
