1b351a92fe9d17ffa92f5e7e3c3e78cdb01d7a38
JuneCho23 <junecho.ucb@gmail.com>
2017-08-15 19:53:13 -0700

    Created RelativeLayout

---
+++ XamUDemy/Layouts/RelativeLayout.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Layouts
+{
+    public partial class RelativeLayout : ContentPage
+    {
+        public RelativeLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
