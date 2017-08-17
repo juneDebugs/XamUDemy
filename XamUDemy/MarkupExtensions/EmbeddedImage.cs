2e1f1b4473736ed4b58c3bf4334e26dbbd877894
JuneCho23 <junecho.ucb@gmail.com>
2017-08-17 10:23:57 -0700

    Created interface for EmbeddedImage

---
+++ XamUDemy/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamUDemy.MarkupExtensions
+{
+    public class EmbeddedImage : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ImageSource.FromResource("XamUDemy.Images.jordan.png");
+        }
+    }
+}
