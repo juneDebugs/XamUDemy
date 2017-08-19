8e909ad3abbcdb76b90e80d823d19052e189859a
JuneCho23 <junecho.ucb@gmail.com>
2017-08-19 11:48:48 -0700

    Created PhotoGalleryPage

---
+++ XamUDemy/Pages/PhotoGalleryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Pages
+{
+    public partial class PhotoGalleryPage : ContentPage
+    {
+        public PhotoGalleryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
