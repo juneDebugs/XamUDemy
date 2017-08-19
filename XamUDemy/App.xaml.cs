5cba0a469c4be6edca7249a9f35e3e2b93b8274e
JuneCho23 <junecho.ucb@gmail.com>
2017-08-19 12:15:35 -0700

    Built PhotoGalleryPage

--- XamUDemy/App.xaml.cs
+++ XamUDemy/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamUDemy
         {
             InitializeComponent();
 
-            MainPage = new ImageCirclePage();
+            MainPage = new PhotoGalleryPage();
         }
 
         protected override void OnStart()
