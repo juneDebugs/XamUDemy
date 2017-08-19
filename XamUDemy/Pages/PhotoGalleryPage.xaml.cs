0a2bd6f79e204c1767cb1750838bc75617c3d236
JuneCho23 <junecho.ucb@gmail.com>
2017-08-19 12:02:00 -0700

    PhotoGalleryPage C#

--- XamUDemy/Pages/PhotoGalleryPage.xaml.cs
+++ XamUDemy/Pages/PhotoGalleryPage.xaml.cs
@@ -7,9 +7,22 @@ namespace XamUDemy.Pages
 {
     public partial class PhotoGalleryPage : ContentPage
     {
+        private int _currentImagedId = 1;
+
         public PhotoGalleryPage()
         {
             InitializeComponent();
+
+            _currentImagedId = 1;
+
+            LoadImage();
         }
+
+
+
+
+
+
+
     }
 }
