3c082a80d7546c71b65da88941ddb57deb3290c3
JuneCho23 <junecho.ucb@gmail.com>
2017-08-19 13:27:39 -0700

    finished TextCell

--- XamUDemy/Lists/ListViewXAML.xaml.cs
+++ XamUDemy/Lists/ListViewXAML.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using XamUDemy.Models;
 
 namespace XamUDemy.Lists
 {
@@ -11,14 +12,23 @@ namespace XamUDemy.Lists
         {
             InitializeComponent();
 
-            var names = new List<string>
-            {
-                "Eminem",
-                "Jimmy",
-                "Dr. Dre"
+            //This is how you hard code a listView 
+            //var names = new List<string>
+            //{
+            //    "Eminem",
+            //    "Jimmy",
+            //    "Dr. Dre"
+            //};
+
+            //listView.ItemsSource = names;
+
+            listView.ItemsSource = new List<Contact>{
+                new Contact { Name = "Eminem", ImageURL = "http://lorempixel.com/100/100/people/1"},
+                new Contact { Name = "Jordan", ImageURL = "http://lorempixel.com/100/100/people/2",
+                    Status= "Hey, let's talk!"},
             };
 
-            listView.ItemsSource = names;
+
         }
     }
 }
