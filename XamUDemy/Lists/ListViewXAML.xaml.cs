6cca0ffd949cab43baf1fa98b257d8059d42a6f8
JuneCho23 <junecho.ucb@gmail.com>
2017-08-19 13:01:17 -0700

    created name list string

---
+++ XamUDemy/Lists/ListViewXAML.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Lists
+{
+    public partial class ListViewXAML : ContentPage
+    {
+        public ListViewXAML()
+        {
+            InitializeComponent();
+
+            var names = new List<string>
+            {
+                "Eminem",
+                "Jimmy",
+                "Dr. Dre"
+            };
+
+
+        }
+    }
+}
