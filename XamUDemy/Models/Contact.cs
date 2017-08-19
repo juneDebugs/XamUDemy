50028c5c346586acf81ca46a86b3ec15970d3ac9
JuneCho23 <junecho.ucb@gmail.com>
2017-08-19 13:13:12 -0700

    Created Contact properties in Model

--- XamUDemy/Models/Contact.cs
+++ XamUDemy/Models/Contact.cs
@@ -3,8 +3,12 @@ namespace XamUDemy.Models
 {
     public class Contact
     {
-        public Contact()
-        {
-        }
+        //These are different type of properties
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public string ImageURL { get; set; }
+
     }
 }
