51942b9e6d6b46d8875694e57c7e9f7a55dff27e
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 08:51:34 -0700

    created ListViewXamlSelections

---
+++ XamUDemy/Lists/ListViewXamlSelections.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Lists
+{
+    public partial class ListViewXamlSelections : ContentPage
+    {
+        public ListViewXamlSelections()
+        {
+            InitializeComponent();
+        }
+    }
+}
