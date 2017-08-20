47cd812f7cfb2579697ecf9c52470709b3d24cf1
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 09:02:24 -0700

    HandleItem Selected and Tapped C# CB

--- XamUDemy/Lists/ListViewXamlSelections.xaml.cs
+++ XamUDemy/Lists/ListViewXamlSelections.xaml.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using XamUDemy.Models;
 
 namespace XamUDemy.Lists
 {
     public partial class ListViewXamlSelections : ContentPage
     {
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            var contact = e.SelectedItem as Contact;
+            DisplayAlert("Selected", contact.Name, "OK");
+        }
+
+        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            var contact= e.Item as Contact;
+            DisplayAlert("Tapped", contact.Name, "OK");
+        }
+
         public ListViewXamlSelections()
         {
             InitializeComponent();
-        }
+
+			listView.ItemsSource = new List<Contact>{
+				new Contact { Name = "Eminem", ImageURL = "http://lorempixel.com/100/100/people/1"},
+				new Contact { Name = "Jordan", ImageURL = "http://lorempixel.com/100/100/people/2",
+					Status= "Hey, let's talk!"},
+			};
+
+		}
     }
 }
