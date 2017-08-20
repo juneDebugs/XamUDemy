7c50972424789a2e349569c35e9b1704d24754f6
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 11:24:25 -0700

    Created SearchService

---
+++ XamUDemy/Services/SearchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace XamUDemy.Services
+{
+    public class SearchService 
+    {
+        private List<Search>
+    }
+}
+
