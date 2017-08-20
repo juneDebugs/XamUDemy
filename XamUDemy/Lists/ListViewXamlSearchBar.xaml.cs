f1c502ad19e0a6f71c6667ac797895e011969ae7
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 10:03:17 -0700

    ListViewXamlSearchBar C#

--- XamUDemy/Lists/ListViewXamlSearchBar.xaml.cs
+++ XamUDemy/Lists/ListViewXamlSearchBar.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
+using XamUDemy.Models;
 
 namespace XamUDemy.Lists
 {
@@ -10,6 +10,12 @@ namespace XamUDemy.Lists
         public ListViewXamlSearchBar()
         {
             InitializeComponent();
+
+			listView.ItemsSource = new List<Contact>{
+				new Contact { Name = "Eminem", ImageURL = "http://lorempixel.com/100/100/people/1"},
+				new Contact { Name = "Jordan", ImageURL = "http://lorempixel.com/100/100/people/2",
+					Status= "Hey, let's talk!"},
+			};
         }
     }
 }
