900c05231fba84cb5acc7e02743320acdb0f5e88
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 10:29:27 -0700

    created AirBnbListViewPage

---
+++ XamUDemy/Pages/AirBnbListViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Pages
+{
+    public partial class AirBnbListViewPage : ContentPage
+    {
+        public AirBnbListViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
