ef6ddbffad0c62c82f6d59a9734a3634a7beac2a
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 11:33:11 -0700

    finished SearchService Service

--- XamUDemy/Services/SearchService.cs
+++ XamUDemy/Services/SearchService.cs
@@ -1,13 +1,36 @@
 ﻿using System;
-
 using Xamarin.Forms;
 using System.Collections.Generic;
+using System.Reflection;
+using XamUDemy.Models;
 
 namespace XamUDemy.Services
 {
-    public class SearchService 
+    public class SearchService
     {
-        private List<Search>
+        private List<Search> _searches = new List<Search>
+        {
+            new Search
+            {
+              Id = 1,
+                Location = "West Hollywood, CA, United States",
+                CheckIn = new DateTime(2017, 1, 8),
+                CheckOut= new DateTime(2017, 7, 8)
+            },
+
+			new Search
+			{
+			  Id = 2,
+				Location = "Santa Monica, CA, United States",
+				CheckIn = new DateTime(2017, 1, 8),
+				CheckOut= new DateTime(2017, 7, 8)
+			},
+
+        };
+
+
+
+
     }
 }
 
