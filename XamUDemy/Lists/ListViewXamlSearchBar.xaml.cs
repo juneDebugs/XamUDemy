724b2308eecac3f7db9d36df79bbb732045ad89c
JuneCho23 <junecho.ucb@gmail.com>
2017-08-20 10:01:52 -0700

    Created ListViewXamlSearchBar

---
+++ XamUDemy/Lists/ListViewXamlSearchBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Lists
+{
+    public partial class ListViewXamlSearchBar : ContentPage
+    {
+        public ListViewXamlSearchBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
