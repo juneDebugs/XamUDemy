9dc55b22497133a7f9eaef73320985a8d05ee729
JuneCho23 <junecho.ucb@gmail.com>
2017-08-21 13:49:24 -0700

    Finished ModalSecondPage

---
+++ XamUDemy/Navigations/ModalSecondPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Navigations
+{
+    public partial class ModalSecondPage : ContentPage
+    {
+        public ModalSecondPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
