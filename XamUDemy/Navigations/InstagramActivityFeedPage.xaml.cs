ed9431cf6e2c65e0e048e9cba8b02027d64f500a
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 12:16:55 -0700

    created UserService Service

--- XamUDemy/Navigations/InstagramActivityFeedPage.xaml.cs
+++ XamUDemy/Navigations/InstagramActivityFeedPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Xamarin.Forms;
 using XamUDemy.Models;
+using XamUDemy.Services;
 
 namespace XamUDemy.Navigations
 {
---
+++ XamUDemy/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Services
+{
+    public class UserService : ContentPage
+    {
+        public UserService()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
