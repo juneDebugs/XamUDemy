f5d94a90c4f16fb3c4d4cf5e4f357cf4bf06acba
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 11:34:33 -0700

    finished InstagramMyProfilePage XAML

---
+++ XamUDemy/Services/ActivityService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Services
+{
+    public class ActivityService : ContentPage
+    {
+        public ActivityService()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
