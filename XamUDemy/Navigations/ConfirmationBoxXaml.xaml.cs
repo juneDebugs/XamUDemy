6c5c4644ae4e64d0c0723b07a9b2d5c8a640cab6
JuneCho23 <junecho.ucb@gmail.com>
2017-08-21 20:19:40 -0700

    created ConfirmationBoxXaml

---
+++ XamUDemy/Navigations/ConfirmationBoxXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Navigations
+{
+    public partial class ConfirmationBoxXaml : ContentPage
+    {
+        public ConfirmationBoxXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
