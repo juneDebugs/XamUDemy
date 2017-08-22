e1557b53274608af7bf0a827f4ee061f5e6bf561
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 11:22:28 -0700

    created InstagramMyProfilePage

---
+++ XamUDemy/Navigations/InstagramMyProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Navigations
+{
+    public partial class InstagramMyProfilePage : ContentPage
+    {
+        public InstagramMyProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
