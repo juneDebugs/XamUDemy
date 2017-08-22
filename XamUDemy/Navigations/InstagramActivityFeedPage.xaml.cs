849bed5c1e20945435f8c19abfc29e66e56958f5
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 10:38:32 -0700

    CleanUp

---
+++ XamUDemy/Navigations/InstagramActivityFeedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Navigations
+{
+    public partial class InstagramActivityFeedPage : ContentPage
+    {
+        public InstagramActivityFeedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
