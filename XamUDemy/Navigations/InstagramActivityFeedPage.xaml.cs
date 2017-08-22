a6b13fb79191c892309bd2fbf62d20544b479952
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 10:54:08 -0700

    created OnActivitySelected in InstagramActivityFeedPage C# CB

--- XamUDemy/Navigations/InstagramActivityFeedPage.xaml.cs
+++ XamUDemy/Navigations/InstagramActivityFeedPage.xaml.cs
@@ -7,9 +7,22 @@ namespace XamUDemy.Navigations
 {
     public partial class InstagramActivityFeedPage : ContentPage
     {
+        private ActivityService _service = new ActivityService();
+
+
         public InstagramActivityFeedPage()
         {
             InitializeComponent();
+
+            activityFeed.ItemsSource = _service.GetActivities();
         }
+
+        void OnActivitySelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+
+
+        }
+
+
     }
 }
