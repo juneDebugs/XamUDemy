c054edce79b69a282e146b8cd9e6db20acd5793b
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 10:23:59 -0700

    ApplicationProperties C# EventHandler for Switch and Event Cell

--- XamUDemy/DataAccess/ApplicationPropertiesXaml.xaml.cs
+++ XamUDemy/DataAccess/ApplicationPropertiesXaml.xaml.cs
@@ -7,6 +7,17 @@ namespace XamUDemy.DataAccess
 {
     public partial class ApplicationPropertiesXaml : ContentPage
     {
+        //Notice that we only have ONE Event handler
+
+        void OnChange(object sender, Xamarin.Forms.ToggledEventArgs e)
+        {
+            //This is for EntryCell
+            Application.Current.Properties["Name"] = title.Text;
+
+            //This is for the switchCell
+            Application.Current.Properties["NotificationsEnabled"] = title.Text;
+        }
+
         public ApplicationPropertiesXaml()
         {
             InitializeComponent();
