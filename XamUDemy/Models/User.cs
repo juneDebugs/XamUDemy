3ab755085c599c1069af648e33122e63b67e8b82
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 11:14:49 -0700

    Created User Model

---
+++ XamUDemy/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Models
+{
+    public class User : ContentPage
+    {
+        public User()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
