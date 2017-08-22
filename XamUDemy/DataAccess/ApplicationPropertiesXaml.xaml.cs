ba6767968efad6a8d5e10ee7fa11e0da1a558930
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 10:02:32 -0700

    created ApplicationPropertiesXaml

---
+++ XamUDemy/DataAccess/ApplicationPropertiesXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.DataAccess
+{
+    public partial class ApplicationPropertiesXaml : ContentPage
+    {
+        public ApplicationPropertiesXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
