7e04742be15a622b24e6816757dee126b4cad256
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 11:13:07 -0700

    created Activity Model

---
+++ XamUDemy/Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Models
+{
+    public class Activity : ContentPage
+    {
+        public Activity()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
