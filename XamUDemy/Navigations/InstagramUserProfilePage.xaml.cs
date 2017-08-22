bbfbaf1ce206dae285c404eedfb62acea5dc9156
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 12:53:51 -0700

    finished InstagramUserProfilePage C# CB

--- XamUDemy/Navigations/InstagramUserProfilePage.xaml.cs
+++ XamUDemy/Navigations/InstagramUserProfilePage.xaml.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using XamUDemy.Services;
 
 namespace XamUDemy.Navigations
 {
     public partial class InstagramUserProfilePage : ContentPage
     {
-        public InstagramUserProfilePage()
+        private UserService _service = new UserService();
+
+        public InstagramUserProfilePage(int userId)
         {
+            BindingContext = _service.GetUser(userId);
+
             InitializeComponent();
         }
     }
