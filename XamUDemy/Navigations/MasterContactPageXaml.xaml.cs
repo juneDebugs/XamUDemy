d936952f4459204d023bc88dadee20d03abb1e55
JuneCho23 <junecho.ucb@gmail.com>
2017-08-21 17:27:43 -0700

    finished MasterContactPage XAML + C#

--- XamUDemy/Navigations/MasterContactPageXaml.xaml.cs
+++ XamUDemy/Navigations/MasterContactPageXaml.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using XamUDemy.Models;
 
 namespace XamUDemy.Navigations
 {
@@ -10,6 +11,12 @@ namespace XamUDemy.Navigations
         public MasterContactPageXaml()
         {
             InitializeComponent();
+
+            listView.ItemsSource = new List<Contact>{
+                new Contact { Name = "Eminem", ImageURL = "http://lorempixel.com/100/100/people/1"},
+                new Contact { Name = "Jordan", ImageURL = "http://lorempixel.com/100/100/people/2",
+                    Status= "Hey, let's talk!"},
+            };
         }
     }
 }
