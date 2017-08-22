a695f8292465da00efb83f34c03ea41966edf760
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 11:20:39 -0700

    Finished Activity Model

--- XamUDemy/Models/Activity.cs
+++ XamUDemy/Models/Activity.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-
 using Xamarin.Forms;
 
 namespace XamUDemy.Models
 {
-    public class Activity : ContentPage
+    public class Activity 
     {
-        public Activity()
+        public int UserId { get; set; }
+        public string Description { get; set; }
+
+        public string ImageURL
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Hello ContentPage" }
-                }
-            };
+            get { return $"http://lorempixel.com/100/100/people/{UserId}"; }
         }
     }
 }
