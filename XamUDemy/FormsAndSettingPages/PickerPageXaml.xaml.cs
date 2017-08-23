e3c57d5a5e080dc7ad55d7cada86af76688731f0
JuneCho23 <junecho.ucb@gmail.com>
2017-08-23 13:45:34 -0700

    created PickerPageXaml

---
+++ XamUDemy/FormsAndSettingPages/PickerPageXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.FormsAndSettingPages
+{
+    public partial class PickerPageXaml : ContentPage
+    {
+        public PickerPageXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
