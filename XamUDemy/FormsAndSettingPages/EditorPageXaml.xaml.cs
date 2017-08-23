20de25da6200efb7cc7c686dd64ae69f8cd686ca
JuneCho23 <junecho.ucb@gmail.com>
2017-08-23 13:36:53 -0700

    created EditorPageXaml

---
+++ XamUDemy/FormsAndSettingPages/EditorPageXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.FormsAndSettingPages
+{
+    public partial class EditorPageXaml : ContentPage
+    {
+        public EditorPageXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
