66bbe1e71a1881f0b5b8bf122898910a3c8176a5
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 18:32:40 -0700

    created SliderPageXaml

---
+++ XamUDemy/FormsAndSettingPages/SliderPageXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.FormsAndSettingPages
+{
+    public partial class SliderPageXaml : ContentPage
+    {
+        public SliderPageXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
