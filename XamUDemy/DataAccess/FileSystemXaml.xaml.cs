6b2b315546bae4bc988b0f61f5f1e6598cf98125
JuneCho23 <junecho.ucb@gmail.com>
2017-08-22 18:00:53 -0700

    created FileSystem

---
+++ XamUDemy/DataAccess/FileSystemXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.DataAccess
+{
+    public partial class FileSystemXaml : ContentPage
+    {
+        public FileSystemXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
