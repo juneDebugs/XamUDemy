5404d7d914bd2749721c492dea567fb9c466ad8d
JuneCho23 <junecho.ucb@gmail.com>
2017-08-23 14:43:14 -0700

    finished  SelectedIndexChanged

--- XamUDemy/FormsAndSettingPages/PickerPageXaml.xaml.cs
+++ XamUDemy/FormsAndSettingPages/PickerPageXaml.xaml.cs
@@ -7,6 +7,12 @@ namespace XamUDemy.FormsAndSettingPages
 {
     public partial class PickerPageXaml : ContentPage
     {
+        void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var contactMethod = contactMethods.Items[contactMethods.SelectedIndex];
+            DisplayAlert("Selection", contactMethod, "OK");
+        }
+
         public PickerPageXaml()
         {
             InitializeComponent();
