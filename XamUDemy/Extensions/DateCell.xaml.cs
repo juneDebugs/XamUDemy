dd2ac319f11a1699d377d3b418cc093daa5e9d98
JuneCho23 <junecho.ucb@gmail.com>
2017-08-24 11:12:04 -0700

    Created DateCell

---
+++ XamUDemy/Extensions/DateCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Extensions
+{
+    public partial class DateCell : ContentPage
+    {
+        public DateCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
