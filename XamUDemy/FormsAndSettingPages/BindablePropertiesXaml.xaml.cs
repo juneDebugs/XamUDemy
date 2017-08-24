8613c04725e813e610f0c2e3f17a23d2e3502b62
JuneCho23 <junecho.ucb@gmail.com>
2017-08-24 11:02:39 -0700

    created BindablePropertiesXaml

---
+++ XamUDemy/FormsAndSettingPages/BindablePropertiesXaml.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.FormsAndSettingPages
+{
+    public partial class BindablePropertiesXaml : ContentPage
+    {
+        public BindablePropertiesXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
