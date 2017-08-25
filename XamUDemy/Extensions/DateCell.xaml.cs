9eba4b2465b46eaa2e6661a024944c79a11f156c
JuneCho23 <junecho.ucb@gmail.com>
2017-08-25 08:42:14 -0700

    created Bindable Infrastructure for Xamarin.Forms

--- XamUDemy/Extensions/DateCell.xaml.cs
+++ XamUDemy/Extensions/DateCell.xaml.cs
@@ -7,7 +7,22 @@ namespace XamUDemy.Extensions
 {
     public partial class DateCell : ViewCell
     {
-        public string Label { get; set; }
+        //This is called Bindable Infrastructure for Xamarin.Forms
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(DateCell));
+
+        public string Label 
+        { 
+            get
+            {
+                return (string)GetValue(LabelProperty);
+            }
+
+            set
+            {
+                SetValue(LabelProperty, value);
+            }
+        }
 
         public DateCell()
         {
