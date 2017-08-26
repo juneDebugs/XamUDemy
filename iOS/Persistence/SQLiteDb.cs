b201af9cc0e7f607db5d90fcc2f1c5ee2b2e3dfb
JuneCho23 <junecho.ucb@gmail.com>
2017-08-26 16:10:00 -0700

    finished SQLiteDb for iOS

--- iOS/Persistence/SQLiteDb.cs
+++ iOS/Persistence/SQLiteDb.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using XamUDemy.Pages;
+using SQLite;
+using Xamarin.Forms;
+using XamUDemy.iOS.Persistence;
+using System.IO;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
 namespace XamUDemy.iOS.Persistence
 {
-    public class SQLiteDb
+    public class SQLiteDb : ISQLiteDb
     {
-        public SQLiteDb()
+        public SQLiteAsyncConnection GetConnection()
         {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+                                           
         }
     }
 }
