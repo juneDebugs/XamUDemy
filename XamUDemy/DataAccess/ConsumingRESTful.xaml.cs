e8b424f0fbd0fad2282bda37104e3f1da28c157f
JuneCho23 <junecho.ucb@gmail.com>
2017-08-27 09:37:13 -0700

    finished ConsumingRESTful XAML

--- XamUDemy/DataAccess/ConsumingRESTful.xaml.cs
+++ XamUDemy/DataAccess/ConsumingRESTful.xaml.cs
@@ -7,6 +7,16 @@ namespace XamUDemy.DataAccess
 {
     public partial class ConsumingRESTful : ContentPage
     {
+        void OnUpdate(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void OnAdd(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         public ConsumingRESTful()
         {
             InitializeComponent();
