94e52ffa4215c0a0e89f8c38739d1be5acd17888
JuneCho23 <junecho.ucb@gmail.com>
2017-08-26 17:35:49 -0700

    added eventHandlers RecipeSQLite

--- XamUDemy/DataAccess/RecipeSQLite.xaml.cs
+++ XamUDemy/DataAccess/RecipeSQLite.xaml.cs
@@ -7,9 +7,20 @@ namespace XamUDemy.DataAccess
 {
     public partial class RecipeSQLite : ContentPage
     {
+
         public RecipeSQLite()
         {
             InitializeComponent();
+        }
+
+		void OnAdd(object sender, System.EventArgs e)
+		{
+			
+		}
+
+        void OnUpdate(object sender, System.EventArgs e)
+        {
+            
         }
     }
 }
