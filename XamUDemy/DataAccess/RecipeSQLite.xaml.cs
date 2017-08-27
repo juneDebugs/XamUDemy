71f2325944cfe9da84a5b8cac41ca92763a1e4d4
JuneCho23 <junecho.ucb@gmail.com>
2017-08-26 17:53:57 -0700

    created ObservableCollection

--- XamUDemy/DataAccess/RecipeSQLite.xaml.cs
+++ XamUDemy/DataAccess/RecipeSQLite.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using XamUDemy.Interface;
 using SQLite;
+using System.Collections.ObjectModel;
 
 namespace XamUDemy.DataAccess
 {
@@ -19,6 +20,8 @@ namespace XamUDemy.DataAccess
     public partial class RecipeSQLite : ContentPage
     {
         private SQLiteAsyncConnection _connection;
+        public ObservableCollection<Recipe> _recipes;
+
         public RecipeSQLite()
         {
             InitializeComponent();
@@ -31,14 +34,16 @@ namespace XamUDemy.DataAccess
 			await _connection.CreateTableAsync<Recipe>();
 			
 			var recipes= await _connection.Table<Recipe>().ToListAsync();
+            _recipes = new ObservableCollection<Recipe>(recipes);
             recipesListView.ItemsSource = recipes;
 
             base.OnAppearing();
         }
 
-		void OnAdd(object sender, System.EventArgs e)
+		async void OnAdd(object sender, System.EventArgs e)
 		{
-			
+            var recipe = new Recipe { Name = "Recipe" + DateTime.Now.Ticks };
+            await _connection.InsertAsync(recipe);
 		}
 
         void OnUpdate(object sender, System.EventArgs e)
