8c2fba4caac19b4246f1717c1e9fe4b95b36e5ee
JuneCho23 <junecho.ucb@gmail.com>
2017-08-27 09:30:42 -0700

    created ConsumingRESTful

---
+++ XamUDemy/DataAccess/ConsumingRESTful.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.DataAccess
+{
+    public partial class ConsumingRESTful : ContentPage
+    {
+        public ConsumingRESTful()
+        {
+            InitializeComponent();
+        }
+    }
+}
