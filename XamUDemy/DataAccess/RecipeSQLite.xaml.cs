62c9eced18fd5e77fa70614bb8edc36b0cc61575
JuneCho23 <junecho.ucb@gmail.com>
2017-08-26 17:40:34 -0700

    finished RecipeSQLite C# CB

--- XamUDemy/DataAccess/RecipeSQLite.xaml.cs
+++ XamUDemy/DataAccess/RecipeSQLite.xaml.cs
@@ -5,9 +5,14 @@ using Xamarin.Forms;
 
 namespace XamUDemy.DataAccess
 {
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public partial class RecipeSQLite : ContentPage
     {
-
         public RecipeSQLite()
         {
             InitializeComponent();
@@ -19,6 +24,11 @@ namespace XamUDemy.DataAccess
 		}
 
         void OnUpdate(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        void OnDelete(object sender, System.EventArgs e)
         {
             
         }
