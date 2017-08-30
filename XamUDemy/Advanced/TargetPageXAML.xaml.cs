9ec9578efe6fc8433cea7fd0deda85c74f5cd144
JuneCho23 <junecho.ucb@gmail.com>
2017-08-30 10:44:49 -0700

    CleanUP

---
+++ XamUDemy/Advanced/TargetPageXAML.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Advanced
+{
+    public partial class TargetPageXAML : ContentPage
+    {
+        public TargetPageXAML()
+        {
+            InitializeComponent();
+        }
+    }
+}
