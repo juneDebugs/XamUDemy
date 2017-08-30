3c10a7a1ccd8daf5e61be6318957b88cfd94b137
JuneCho23 <junecho.ucb@gmail.com>
2017-08-30 10:50:25 -0700

    finished TargetPageXAML C# CB

--- XamUDemy/Advanced/TargetPageXAML.xaml.cs
+++ XamUDemy/Advanced/TargetPageXAML.xaml.cs
@@ -7,9 +7,16 @@ namespace XamUDemy.Advanced
 {
     public partial class TargetPageXAML : ContentPage
     {
+        public event EventHandler<double> SliderValueChanged;
+
         public TargetPageXAML()
         {
             InitializeComponent();
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            SliderValueChanged?.Invoke(this, e.NewValue);
         }
     }
 }
