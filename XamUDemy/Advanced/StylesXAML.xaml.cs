67d03a2092993ac38ad514570789149d314b76fc
JuneCho23 <junecho.ucb@gmail.com>
2017-08-29 19:06:59 -0700

    created StylesXAML

---
+++ XamUDemy/Advanced/StylesXAML.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamUDemy.Advanced
+{
+    public partial class StylesXAML : ContentPage
+    {
+        public StylesXAML()
+        {
+            InitializeComponent();
+        }
+    }
+}
